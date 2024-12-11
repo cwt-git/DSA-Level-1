7e3407b62510875449526de483f36a4a2ff180c5
cwt-git <codewithtanuj@outlook.com>
2024-12-11 23:13:48 +0530

    if else

--- conditions.java
+++ conditions.java
@@ -1,3 +1,5 @@
+
+
 public class conditions {
     public static void main(String[] args) {
         int x = 12;
@@ -7,5 +9,17 @@ public class conditions {
             System.out.println("False");
         }
         System.out.println("This will print always");
+
+        // new Section
+        int n1 = 12;
+        int n2 = 13;
+
+        if (n1 == n2) {
+            System.out.println("Equal");
+        } else if (n1 < n2) {
+            System.out.println("n1<n2");
+        } else {
+            System.out.println("n2<n1");
+        }
     }
 }
