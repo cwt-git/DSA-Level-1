e04d8e0ecb595a9ccfceab5ebe28c6eb25a38d57
cwt-git <codewithtanuj@outlook.com>
2024-12-12 00:14:43 +0530

    loops

---
+++ loops.java
@@ -0,0 +1,16 @@
+public class loops {
+    public static void main(String[] args) {
+        // for Loop
+        int n = 15;
+        for (int i = 0; i < n; i++) {
+            System.out.println("hello" + i);
+        }
+        // While loop
+        int i = 1;
+        while (i <= 9) {
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("I am out of while loop");
+    }
+}
