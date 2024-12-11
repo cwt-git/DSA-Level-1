319bd3d70c0e046707d69d169931799717417ff6
cwt-git <codewithtanuj@outlook.com>
2024-12-11 23:05:11 +0530

    if Else

---
+++ conditions.java
@@ -0,0 +1,11 @@
+public class conditions {
+    public static void main(String[] args) {
+        int x = 12;
+        if (x % 2 == 0) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+        System.out.println("This will print always");
+    }
+}
